33bb027c4cd02e2ca8024a40b7c5a0ccc6293503
Adhiraj Sen <adhiraj001@gmail.com>
2020-11-19 16:05:47 -0600

    Addition of top 250 movies df to innit.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 import os.path
 from flask_login import LoginManager
+import imdb
+import pandas as pd
+import imdb.helpers
 
 
 
@@ -22,6 +25,31 @@ loginManager.login_view = 'Login'
 loginManager.login_message_category = 'info'
 from app import routes
 
+movies = imdb.IMDb()
+
+#assign "get top 250" function to variable search
+search_top = movies.get_top250_movies()
+
+#assing key:value to dict, moviesDF_top{'id': 'name'}
+moviesDF_top = pd.DataFrame(columns = ['id', 'title'])
+for name in search_top:
+	ids = name.movieID 
+	moviesDF_top = moviesDF_top.append({'id' : ids, 'title': str(name) }, ignore_index=True)
+"""
+poster_list = []
+for name in search_top:
+	ids = name.movieID 
+	movieAccess = movies.get_movie(ids)
+	poster_list += movieAccess.data['cover url']
+moviesDF_top['poster_path'] = poster_list
+print(moviesDF_top['poster_path'])
+"""
+"""
+posterDF = pd.DataFrame(columns = ['poster_path'])
+for name in search_top:
+	m = movies.get_movie(name.movieID) # Avatar.
+	posterDF = posterDF.append({'poster_path': imdb.helpers.fullSizeCoverURL(m)}, ignore_index=True)
+"""
 # if the SQL database already exists, don't create another one.
 if os.path.exists('site.db'):
     pass
