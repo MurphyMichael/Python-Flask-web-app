d9d84964d9fda57ef4746f1924b45433d1037185
Michael <mwmurphy1029@gmail.com>
2020-12-03 13:50:40 -0600

    made secrets.py that won't be commited

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_mail import Mail
 import imdb
 import pandas as pd
 import imdb.helpers
+import secrets as secret
 
 # no changes
 
@@ -27,8 +28,8 @@ loginManager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'UNT3444group4@gmail.com'
-app.config['MAIL_PASSWORD']	= 'QWERTYpassword123'
+app.config['MAIL_USERNAME'] = secret.USER
+app.config['MAIL_PASSWORD']	= secret.PASS
 mail = Mail(app)
 
 from app import routes
